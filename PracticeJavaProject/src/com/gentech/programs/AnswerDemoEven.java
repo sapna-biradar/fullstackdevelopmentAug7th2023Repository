cd9fb30bd7fc09caa2f82926b796c8fe0125f362
sapna-biradar <sapnabiradar80@gmail.com>
2023-09-25 13:34:22 +0530

    singleton

---
+++ PracticeJavaProject/src/com/gentech/programs/AnswerDemoEven.java
@@ -0,0 +1,15 @@
+package com.gentech.programs;
+
+public class AnswerDemoEven {
+	public static void main(String[] args) {
+		for (int i=30;i<=40;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.print(i+" ");
+			}
+		}
+	}
+
+}
+
